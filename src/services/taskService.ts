32784d10a1e1c705528e551dc2b89424cd3d7068
MarcoAguirre <viniciosoria16@gmail.com>
2025-08-02 02:45:01 -0500

    refactor: Use environment variables for API URL and KEY

--- src/services/taskService.ts
+++ src/services/taskService.ts
@@ -1,7 +1,7 @@
 import type { Task } from '../types/task';
 
-const API_URL = 'https://zctpzslxpvkuuxxsogco.supabase.co/rest/v1/tasks';
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjdHB6c2x4cHZrdXV4eHNvZ2NvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5NzEzNDAsImV4cCI6MjA2OTU0NzM0MH0.hWttk7qTfmGabO2_YaCWljodRyhi-5jbRV9q2Y_XnKI';
+const API_URL = import.meta.env.VITE_API_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 export async function getTasks(): Promise<Task[]> {
   const response = await fetch(`${API_URL}?select=*`, {
