51575fd9db6baf1ba00c984a16b419e88e3b4382
MarcoAguirre <viniciosoria16@gmail.com>
2025-08-02 02:13:07 -0500

    feat: setup project with Vite, Tailwind, ESLint and Prettier

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <button className="bg-violet-500 hover:bg-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700 ...">Save changes</button>
+    </>
+  );
+}
+
+export default App;
