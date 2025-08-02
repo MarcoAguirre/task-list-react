82605eccccb98cde222123b15e98d672734eda32
MarcoAguirre <viniciosoria16@gmail.com>
2025-08-02 02:56:42 -0500

    feat: Use GroupedTaskList in app layout; By categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './index.css';
-import { TaskList } from './components/TaskList';
+import { GroupedTaskList } from './components/GroupedTaskList';
 
 function App() {
   return (
     <main className="min-h-screen bg-gray-100">
       <h1 className="text-2xl font-bold text-center py-6">Lista de Tareas</h1>
-      <TaskList />
+      <GroupedTaskList />
     </main>
   );
 }
